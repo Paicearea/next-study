aac414196f9a717c2cd04e24c2d6b2ac136c857c
paice <qocodms20010302@gmail.com>
2024-10-25 13:40:01 +0900

    section 9

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,15 @@
-import ProductList from '@/components/ProductList';
-import BasicLayout from '@/layouts/BasicLayout';
+import ProductHeader from "@/components/ProductHeader";
+import ProductList from "@/components/ProductList";
+import BasicLayout from "@/layouts/BasicLayout";
 
 function ProductPage() {
-	return (
-		<BasicLayout>
-			<h1>상품 목록</h1>
-			<ProductList></ProductList>
-		</BasicLayout>
-	);
+  const headerTitle = "상품 목록 페이지";
+  return (
+    <BasicLayout>
+      <ProductHeader title={headerTitle} />
+      <ProductList></ProductList>
+    </BasicLayout>
+  );
 }
 
 export default ProductPage;
--- pages/products/[id].jsx
+++ pages/products/[id].jsx
@@ -1,33 +1,38 @@
-import { fetchProductById } from '@/api';
-import ProductDetail from '@/components/ProductDetail';
-import BasicLayout from '@/layouts/BasicLayout';
-import React from 'react';
+// 동적 라우팅 하는 파일
+
+import { fetchProductById } from "@/api";
+import ProductDetail from "@/components/ProductDetail";
+import ProductHeader from "@/components/ProductHeader";
+import BasicLayout from "@/layouts/BasicLayout";
+import React from "react";
 
 function ProductDetailPage({ product }) {
-	const detail = product[0];
+  const detail = product[0];
+  const headerDetail = "상품 상세 정보 페이지";
 
-	return (
-		<BasicLayout>
-			<ProductDetail product={detail}></ProductDetail>
-		</BasicLayout>
-	);
+  return (
+    <BasicLayout>
+      <ProductHeader title={headerDetail} />
+      <ProductDetail product={detail}></ProductDetail>
+    </BasicLayout>
+  );
 }
 
 export async function getServerSideProps(context) {
-	try {
-		const { data } = await fetchProductById(context.params.id);
-		return {
-			props: {
-				product: data,
-			},
-		};
-	} catch (error) {
-		return {
-			props: {
-				product: {},
-			},
-		};
-	}
+  try {
+    const { data } = await fetchProductById(context.params.id);
+    return {
+      props: {
+        product: data,
+      },
+    };
+  } catch (error) {
+    return {
+      props: {
+        product: {},
+      },
+    };
+  }
 }
 
 export default ProductDetailPage;
