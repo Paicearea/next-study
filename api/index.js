52046a0a8940bb03cb1206af5b83e5cdd3f66338
paice <qocodms20010302@gmail.com>
2024-10-27 19:28:39 +0900

    상품 리스트 UI 수정

--- api/index.js
+++ api/index.js
@@ -1,42 +1,43 @@
-import axios from 'axios';
+// API 함수 공통화 및 구조화
+import axios from "axios";
 
 const instance = axios.create({
-	baseURL: 'http://localhost:4000',
+  baseURL: "http://localhost:4000",
 });
 
 function fetchProducts() {
-	return instance.get(`/products`);
+  return instance.get(`/products`);
 }
 
 function fetchProductById(id) {
-	return instance.get('/products', {
-		params: {
-			id,
-		},
-	});
+  return instance.get("/products", {
+    params: {
+      id,
+    },
+  });
 }
 
 function fetchCartItems() {
-	return instance.get('/carts');
+  return instance.get("/carts");
 }
 
 function createCartItem({ id, name, price, imageUrl }) {
-	return instance.post('/carts', {
-		id,
-		name,
-		price,
-		imageUrl,
-	});
+  return instance.post("/carts", {
+    id,
+    name,
+    price,
+    imageUrl,
+  });
 }
 
 function removeCartItem(id) {
-	return instance.delete(`/carts/${id}`);
+  return instance.delete(`/carts/${id}`);
 }
 
 export {
-	fetchProducts,
-	fetchProductById,
-	fetchCartItems,
-	createCartItem,
-	removeCartItem,
+  fetchProducts,
+  fetchProductById,
+  fetchCartItems,
+  createCartItem,
+  removeCartItem,
 };
